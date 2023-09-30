652fe8efc063afa2284e92772fdb030122852dbe
TwilightLogic <twilightlogic@gmail.com>
2023-09-30 15:41:16 +0800

    Get categories from database and rendering

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,12 +1,16 @@
-import { SearchInput } from "@/components/search-input"
-import { UserButton } from "@clerk/nextjs"
+import { Categories } from '@/components/categories';
+import { SearchInput } from '@/components/search-input';
+import prismadb from '@/lib/prismadb';
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany();
+
   return (
-    <div className="h-full p-4 space-y-2">
+    <div className='h-full p-4 space-y-2'>
       <SearchInput />
+      <Categories data={categories} />
     </div>
-  )
-}
+  );
+};
 
-export default RootPage
+export default RootPage;
