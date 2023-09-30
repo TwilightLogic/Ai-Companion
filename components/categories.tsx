dba0a11f3c0fb9483a6f43de07ad4ac2771c68c4
TwilightLogic <twilightlogic@gmail.com>
2023-09-30 16:05:44 +0800

    Add category-based routing and dynamic styling

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,17 +1,37 @@
 'use client';
 
+import qs from 'query-string';
 import { cn } from '@/lib/utils';
 import { Category } from '@prisma/client';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface CategoriesProps {
   data: Category[];
 }
 
 export const Categories = ({ data }: CategoriesProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const categoryId = searchParams.get('categoryId');
+  const onClick = (id: string | undefined) => {
+    const query = { categoryId: id };
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  };
+
   return (
     <div className='w-full overflow-x-auto space-x-2 flex p-1'>
       <button
-        className={cn(`
+        onClick={() => onClick(undefined)}
+        className={cn(
+          `
         flex
         items-center
         text-center
@@ -25,14 +45,18 @@ export const Categories = ({ data }: CategoriesProps) => {
         bg-primary/10
         hover:opacity-75
         transition
-      `)}
+      `,
+          !categoryId ? 'bg-primary/25' : 'bg-primary/10'
+        )}
       >
         Newest
       </button>
       {data.map((item) => (
         <button
+          onClick={() => onClick(item.id)}
           key={item.id}
-          className={cn(`
+          className={cn(
+            `
         flex
         items-center
         text-center
@@ -46,7 +70,9 @@ export const Categories = ({ data }: CategoriesProps) => {
         bg-primary/10
         hover:opacity-75
         transition
-      `)}
+      `,
+            item.id === categoryId ? 'bg-primary/25' : 'bg-primary/10'
+          )}
         >
           {item.name}
         </button>
