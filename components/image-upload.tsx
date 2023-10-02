cf2bd49aeca5e67ed03c7bf9862b4afcc90a3310
TwilightLogic <twilightlogic@gmail.com>
2023-10-02 12:54:20 +0800

    Image upload function done ✨

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -23,13 +23,17 @@ export const ImageUpload = ({ value, onChange, disable }: ImageUploadProps) => {
 
   return (
     <div className='space-y-4 h-full flex flex-col justify-center items-center'>
-      <CldUploadButton options={{ maxFiles: 1 }} uploadPreset='mthueqth'>
+      <CldUploadButton
+        onUpload={(result: any) => onChange(result.info.secure_url)}
+        options={{ maxFiles: 1 }}
+        uploadPreset='mthueqth'
+      >
         <div className='p-4 border-4 border-dashed border-primary/10 rounded-lg hover: opacity-75 transition flex flex-col space-y-2 items-center justify-center'>
           <div className='relative h-40 w-40'>
             <Image
               fill
-              alt='U pload'
-              src='/placeholder.svg'
+              alt='Upload'
+              src={value || '/placeholder.svg'}
               className='rounded-lg object-cover'
             />
           </div>
