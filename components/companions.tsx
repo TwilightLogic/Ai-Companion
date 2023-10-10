cb4840843967850f5b233323314a25c8f3f61f56
TwilightLogic <twilightlogic@gmail.com>
2023-10-10 18:37:04 +0800

    Finished companion list

--- components/companions.tsx
+++ components/companions.tsx
@@ -1,5 +1,8 @@
 import { Companion } from '@prisma/client';
 import Image from 'next/image';
+import { Card, CardFooter, CardHeader } from './ui/card';
+import Link from 'next/link';
+import { MessageSquare } from 'lucide-react';
 
 // 每个元素都是 Companion 类型的对象，并且必须包含 _count 属性，该属性的值是一个具有 message 属性的数字对象。
 interface CompanionProps {
@@ -13,9 +16,41 @@ export const Companions = ({ data }: CompanionProps) => {
         <div className='relative w-60 h-60'>
           <Image fill className='grayscale' alt='empty' src='/empty.png' />
         </div>
+        <p className='text-sm text-muted-foreground'>No companions found.</p>
       </div>
     );
   }
 
-  return <div>123</div>;
+  return (
+    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10'>
+      {data.map((item) => (
+        <Card
+          key={item.id}
+          className='bg-primary/10 rounded-xl cursor-pointer hover:opacity-75 transition border-0'
+        >
+          <Link href={`/chat/${item.id}`}>
+            <CardHeader className='flex items-center justify-center text-center text-muted-foreground'>
+              <div className='relative w-32 h-32'>
+                <Image
+                  src={item.src}
+                  fill
+                  className='rounded-xl object-cover'
+                  alt='Companion'
+                />
+              </div>
+              <h3 className='font-bold'>{item.name}</h3>
+              <p className='text-xs'>{item.description}</p>
+            </CardHeader>
+            <CardFooter className='flex items-center justify-between text-xs text-muted-foreground'>
+              <p className='lowercase'>@{item.userName}</p>
+              <div className='flex items-center'>
+                <MessageSquare className='w-3 h-3 mr-1' />
+                {item._count.message}
+              </div>
+            </CardFooter>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  );
 };
